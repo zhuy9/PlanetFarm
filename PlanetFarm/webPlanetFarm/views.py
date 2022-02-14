776b9281e47b214f192a24b0743b5f39ca7def0f
Wenze Ma <maw1@rose-hulman.edu>
2022-02-14 10:03:58 -0500

    update layout

--- PlanetFarm/webPlanetFarm/views.py
+++ PlanetFarm/webPlanetFarm/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-
+from django.contrib import messages
 
 # Create your views here.
 from .models import PlanetFarm
 
 
 def home(request):
+    messages.info(request, 'Hi!')
+    my_farm = PlanetFarm(5, 8, 10)
+    return render(request, 'home.html', {'tiles': my_farm.tiles})
+
+
+def test(request):
     my_farm = PlanetFarm(5, 8, 10)
     return render(request, 'home.html', {'tiles': my_farm.tiles})
