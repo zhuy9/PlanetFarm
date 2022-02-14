3764c9af314f7fa2ae0155a07614f32be1778594
Wenze Ma <maw1@rose-hulman.edu>
2022-02-14 11:44:28 -0500

    Fixed a bug

--- PlanetFarm/webPlanetFarm/views.py
+++ PlanetFarm/webPlanetFarm/views.py
@@ -4,11 +4,13 @@ from django.contrib import messages
 # Create your views here.
 from .models import PlanetFarm
 
-texts = 'hello\n'
+texts = ''
 my_farm = PlanetFarm(5, 8, 10)
 
 
 def home(request):
+    global texts
+    texts = 'Welcome to Planet Farm! Try to enter a sentence below.\n'
     return render(request, 'home.html', {'tiles': my_farm.tiles, 'texts': texts})
 
 
@@ -16,8 +18,9 @@ def test(request):
     global texts
     if request.method == 'POST':
         text_input = request.POST['text_input']
-        texts += text_input + '\n'
-        texts += process(text_input)
+        if text_input != '':
+            texts += text_input + '\n'
+            texts += process(text_input)
     return render(request, 'home.html', {'tiles': my_farm.tiles, 'texts': texts})
 
 
