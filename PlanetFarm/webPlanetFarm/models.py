545b5fd86a02a93f1a725b4ac1f59566305b7347
Yuchen Zhu <42822943+zhuy9@users.noreply.github.com>
2022-02-15 03:28:26 -0500

    can add animals by rand pos

--- PlanetFarm/webPlanetFarm/models.py
+++ PlanetFarm/webPlanetFarm/models.py
@@ -1,15 +1,98 @@
 from django.db import models
-
+import enum
+import random
 
 # Create your models here.
+
+
+class AnimalType(enum.Enum):
+    wolf = "wolf"
+    sheep = "sheep"
+    rabbit = "rabbit"
+    eagle = "eagle"
+    snake = "snake"
+
+
+class TileType(enum.Enum):
+    GRASS = "G"
+    PLAIN = "_"
+    WALL = "W"
+
+
 class Tile:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.type = TileType.PLAIN
+
+    def simple_name(self):
+        return self.type.value
+
+
+class Animal:
+    def __init__(self, type: AnimalType):
+        self.type = type
+        self.x = 0
+        self.y = 0
+        self.move_speed = 1
+
+    def update_position(self, x, y):
+        self.x = x
+        self.y = y
+
+    def random_position(self, row_upper, col_upper):
+        self.x = random.randint(0, row_upper - 1)
+        self.y = random.randint(0, col_upper - 1)
+
+    def simple_name(self):
+        return self.type[0]
+
+    def random_move(self):
+        self.x += 1
+
+    def toString(self):
+        return "I am a {} at ({},{})".format(self.type, self.x, self.y)
 
 
 class PlanetFarm:
     def __init__(self, rows, cols, tile_size):
         self.rows = rows
         self.cols = cols
-        self.tiles = [[Tile(j * tile_size, i * tile_size) for j in range(self.cols)] for i in range(self.rows)]
+        self.animals = []
+        self.tiles = [[Tile(j * tile_size, i * tile_size)
+                       for j in range(self.cols)] for i in range(self.rows)]
+
+    def get_rows(self):
+        return self.rows
+
+    def get_cols(self):
+        return self.cols
+
+    def contains_animal(self, x, y):
+        for ani in self.animals:
+            if ani.x == x and ani.y == y:
+                return True
+        return False
+
+    def toString(self):
+        map = ''
+        for row in range(self.rows):
+            for col in range(self.cols):
+                contain_animal = False
+                for ani in self.animals:
+                    if ani.x == row and ani.y == col:
+                        map += ani.simple_name() + " "
+                        contain_animal = True
+                if not contain_animal:
+                    map += self.tiles[row][col].simple_name() + " "
+            map += '\n'
+        return map
+
+    def update(self):
+        for ani in self.animals:
+            ani.random_move()
+
+    def add_animal(self, animal, random=False):
+        if random:
+            animal.random_position(self.get_rows(), self.get_cols())
+        self.animals.append(animal)
--- PlanetFarm/webPlanetFarm/views.py
+++ PlanetFarm/webPlanetFarm/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
-from .models import PlanetFarm
+from .models import Animal, AnimalType, PlanetFarm
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
+import re
 
 texts = ''
 my_farm = PlanetFarm(5, 8, 10)
+
+
+def train():
+    chatbot = ChatBot('my_bot')
+    trainer = ChatterBotCorpusTrainer(chatbot)
+    trainer.train(
+        "chatterbot.corpus.english"
+    )
+    return chatbot
+
+
+chatbot = train()
 
 
 def home(request):
@@ -20,6 +33,8 @@ def test(request):
         if text_input != '':
             texts += '> ' + text_input + '\n'
             texts += process(text_input)
+    print(my_farm.toString())
+    print(my_farm.animals)
     return render(request, 'home.html', {'tiles': my_farm.tiles, 'texts': texts})
 
 
@@ -29,15 +44,21 @@ def reset():
 
 
 def process(text):
-    chatbot = train()
+    answer = regex_parser(text)
+    if answer:
+        return answer + "\n"
+    global chatbot
     response = chatbot.get_response(text)
     return str(response) + "\n"
 
 
-def train():
-    chatbot = ChatBot('my_bot')
-    trainer = ChatterBotCorpusTrainer(chatbot)
-    trainer.train(
-        "chatterbot.corpus.english"
-    )
-    return chatbot
+def regex_parser(text):
+    text = text.lower()
+    print([val.value for val in (AnimalType)])
+    animal_strings = [val.value for val in (AnimalType)]
+    animal = re.search("|".join(animal_strings), text)
+    if(animal):
+        new_animal = Animal(animal.group())
+        my_farm.add_animal(new_animal, True)
+        print(new_animal.toString())
+        return "animal exists"
