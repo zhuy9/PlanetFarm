81545e67499c757ad1fe41228a459fcc03384a13
Wenze Ma <maw1@rose-hulman.edu>
2022-02-16 23:44:50 -0500

    more

--- PlanetFarm/webPlanetFarm/models.py
+++ PlanetFarm/webPlanetFarm/models.py
@@ -2,6 +2,7 @@ from django.db import models
 import enum
 import random
 
+
 # Create your models here.
 
 
@@ -10,7 +11,7 @@ class AnimalType(enum.Enum):
     sheep = "sheep"
     rabbit = "rabbit"
     eagle = "eagle"
-    snake = "Snake"
+    snake = "snake"
 
 
 class TileType(enum.Enum):
@@ -28,6 +29,9 @@ class Tile:
     def simple_name(self):
         return self.type.value
 
+    def url(self):
+        return "/static/image/" + self.type.value + ".png"
+
 
 class Animal:
     def __init__(self, type: AnimalType):
@@ -41,9 +45,12 @@ class Animal:
         self.y = y
 
     def simple_name(self):
-        return self.type[0]
+        return str(self.type)[0]
 
-    def random_move(self, farm):
+    def url(self):
+        return "/static/image/" + str(self.type) + ".png"
+
+    def random_move(self, farm, dict):
         new_x = self.x
         new_y = self.y
         dir = 1
@@ -54,12 +61,21 @@ class Animal:
         else:
             new_y += dir * self.move_speed
 
-        if farm.valid_pos(new_x, new_y) and not farm.is_occupied(new_x, new_y):
+        if farm.valid_pos(new_x, new_y):
+            dict[(self.x, self.y)].remove(self)
             self.x = new_x
             self.y = new_y
+            add_to_dict(dict, self.x, self.y, self)
 
     def toString(self):
         return "I am a {} at ({},{})".format(self.type, self.x, self.y)
+
+
+def add_to_dict(dict, x, y, obj):
+    if (x, y) in dict:
+        dict[(x, y)].append(obj)
+    else:
+        dict[(x, y)] = [obj]
 
 
 class PlanetFarm:
@@ -69,6 +85,9 @@ class PlanetFarm:
         self.animals = []
         self.tiles = [[Tile(j * tile_size, i * tile_size)
                        for j in range(self.cols)] for i in range(self.rows)]
+        self.values = self.toArray()
+        self.grasses = []
+        self.dict = {}
 
     def get_rows(self):
         return self.rows
@@ -77,13 +96,31 @@ class PlanetFarm:
         return self.cols
 
     def valid_pos(self, x, y):
-        return 0 <= x and 0 <= y and x < self.rows and y < self.cols and self.tiles[x][y].type != TileType.WALL
+        return 0 <= x < self.rows and 0 <= y < self.cols and self.tiles[x][y].type != TileType.WALL
 
-    def is_occupied(self, x, y):
-        for ani in self.animals:
-            if ani.x == x and ani.y == y:
-                return True
-        return False
+    def is_occupied(self, x, y, animal):
+        position = (x, y)
+        if position not in self.dict:
+            return False
+        objs = self.dict[position]
+        add_to_dict(self.dict, x, y, animal)
+        if 'grass' in objs:
+            for animal in objs:
+                if not isinstance(animal, str):
+                    return not (str(animal.type) == 'sheep' or str(animal.type) == 'rabbit')
+        dict = {
+            'sheep': None,
+            'eagle': None,
+            'rabbit': None,
+            'snake': None,
+            'wolf': None
+        }
+        for animal in objs:
+            for ani in dict:
+                if str(animal.type) == ani:
+                    dict[ani] = animal
+        self.dict[position].remove(animal)
+        return not ((dict['sheep'] and dict['wolf']) or (dict['eagle'] and dict['snake']) or (dict['eagle'] and dict['rabbit']) or (dict['snake'] and dict['rabbit']))
 
     def toString(self):
         map = ''
@@ -99,30 +136,101 @@ class PlanetFarm:
             map += '\n'
         return map
 
+    def toArray(self):
+        arr = [['' for _ in range(self.cols)] for _ in range(self.rows)]
+        for row in range(self.rows):
+            for col in range(self.cols):
+                contain_animal = False
+                for ani in self.animals:
+                    if ani.x == row and ani.y == col:
+                        arr[row][col] = ani.url()
+                        contain_animal = True
+                if not contain_animal:
+                    arr[row][col] = self.tiles[row][col].url()
+        return arr
+
     def update(self):
         for ani in self.animals:
-            ani.random_move(self)
+            ani.random_move(self, self.dict)
+        self.values = self.toArray()
+        for position in self.dict:
+            objs = self.dict[position]
+            if 'grass' in objs:
+                for animal in objs:
+                    if not isinstance(animal, str):
+                        if str(animal.type) == 'sheep' or str(animal.type) == 'rabbit':
+                            self.dict[position].remove('grass')
+                            self.tiles[position[0]][position[1]].type = TileType.PLAIN
+                continue
+            dict = {
+                'sheep': None,
+                'eagle': None,
+                'rabbit': None,
+                'snake': None,
+                'wolf': None
+            }
+            for animal in objs:
+                for ani in dict:
+                    if str(animal.type) == ani:
+                        dict[ani] = animal
+
+            if dict['sheep'] and dict['wolf']:
+                self.dict[position].remove(dict['sheep'])
+                self.animals.remove(dict['sheep'])
+            elif dict['eagle'] and dict['snake']:
+                self.dict[position].remove(dict['snake'])
+                self.animals.remove(dict['snake'])
+            elif dict['eagle'] and dict['rabbit']:
+                self.dict[position].remove(dict['rabbit'])
+                self.animals.remove(dict['rabbit'])
+            elif dict['snake'] and dict['rabbit']:
+                self.dict[position].remove(dict['rabbit'])
+                self.animals.remove(dict['rabbit'])
+        print(self.dict)
 
     def random_position(self, animal):
         new_x = random.randint(0, self.rows - 1)
         new_y = random.randint(0, self.cols - 1)
 
-        while self.is_occupied(new_x, new_y):
+        while self.is_occupied(new_x, new_y, animal):
             new_x = random.randint(0, self.rows - 1)
             new_y = random.randint(0, self.cols - 1)
 
         animal.x = new_x
         animal.y = new_y
 
-    def add_animal(self, animal, random=False):
+    def add_animal(self, animal, random=False, coordinate=(1, 1)):
         if random:
             self.random_position(animal)
+        else:
+            animal.x = coordinate[0] - 1
+            animal.y = coordinate[1] - 1
         self.animals.append(animal)
+        add_to_dict(self.dict, animal.x, animal.y, animal)
 
     def add_grass_randomly(self):
         row = random.randint(0, self.rows - 1)
         col = random.randint(0, self.cols - 1)
         self.tiles[row][col].type = TileType.GRASS
+        self.grasses.append((row, col))
+        add_to_dict(self.dict, row, col, 'grass')
+
+    def add_grass(self, coordinate=(1, 1)):
+        self.tiles[coordinate[0] - 1][coordinate[1] - 1].type = TileType.GRASS
+        self.grasses.append((coordinate[0] - 1, coordinate[1] - 1))
+        add_to_dict(self.dict, coordinate[0] - 1, coordinate[1] - 1, 'grass')
 
     def add_fence_randomly(self):
-        return 0
+        row = random.randint(0, self.rows - 1)
+        col = random.randint(0, self.cols - 1)
+        self.tiles[row][col].type = TileType.WALL
+
+    def add_fence(self, coordinate=(1, 1)):
+        self.tiles[coordinate[0] - 1][coordinate[1] - 1].type = TileType.WALL
+
+    def get_animals(self):
+        print([str(animal.type) for animal in self.animals])
+        return [str(animal.type) for animal in self.animals]
+
+    def get_grasses(self):
+        return self.grasses
--- PlanetFarm/webPlanetFarm/views.py
+++ PlanetFarm/webPlanetFarm/views.py
@@ -4,6 +4,9 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
 texts = ''
 my_farm = PlanetFarm(5, 8, 10)
@@ -23,7 +26,7 @@ chatbot = train()
 
 def home(request):
     reset()
-    return render(request, 'home.html', {'tiles': my_farm.tiles, 'texts': texts})
+    return render(request, 'home.html', {'tiles': my_farm.values, 'texts': texts})
 
 
 def test(request):
@@ -36,16 +39,18 @@ def test(request):
     print(my_farm.toString())
     print(my_farm.animals)
     my_farm.update()
-    return render(request, 'home.html', {'tiles': my_farm.tiles, 'texts': texts})
+    return render(request, 'home.html', {'tiles': my_farm.values, 'texts': texts})
 
 
 def reset():
     global texts
     texts = 'Welcome to Planet Farm! Try to enter a sentence below.\n'
+    global my_farm
+    my_farm = PlanetFarm(5, 8, 10)
 
 
 def process(text):
-    answer = regex_parser(text)
+    answer = regex_parser(tokenize(text))
     if answer:
         return answer + "\n"
     global chatbot
@@ -53,25 +58,117 @@ def process(text):
     return str(response) + "\n"
 
 
+def tokenize(text):
+    tokens = nltk.word_tokenize(text)
+    tokens = [word for word in tokens if (word not in stopwords.words()) or word == 'up' or word == 'down']
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    print(tokens)
+    return tokens
+
+
 def regex_parser(text):
-    text = text.lower()
-    # print([val.value for val in (AnimalType)])
-    animal_strings = [val.value for val in (AnimalType)]
-    animal = re.search("|".join(animal_strings), text)
-    if animal:
-        new_animal = Animal(animal.group())
-        my_farm.add_animal(new_animal, True)
-        print(new_animal.toString())
-        return "contains animal"
+    animal_strings = ['wolf', 'sheep', 'rabbit', 'snake', 'eagle']
+    add_strings = ['add', 'create', 'creates', 'creating', 'adding',
+                   'generates', 'generating', 'generate', 'generated',
+                   'created']
+    fence_strings = ['fence', 'wall']
+    eat_strings = ['eat', 'eating', 'eats', 'hunt', 'hunting', 'ate', 'get', 'getting']
+    herbivores = ['sheep', 'rabbit']
+    food_chain = {
+        'wolf': ['sheep'],
+        'eagle': ['snake', 'rabbit'],
+        'snake': ['rabbit']
+    }
+    food_chain2 = {
+        'sheep': ['wolf'],
+        'rabbit': ['eagle', 'snake'],
+        'snake': ['eagle']
+    }
 
-    grass = re.search("grass", text)
-    if grass:
-        my_farm.add_grass_randomly()
-        print("grass added")
-        return "contains grass"
+    animals = []
+    is_adding = False
+    grass = False
+    fence = False
+    coordinates = []
+    x_coordinate = -1
+    to_eat = False
 
-    fence = re.search("fence|wall", text)
-    if fence:
-        my_farm.add_fence_randomly()
-        print("fence added")
-        return "contains fence"
+    for word in text:
+        if word in animal_strings:
+            animals.append(word)
+        if word in add_strings:
+            is_adding = True
+        if word == 'grass':
+            grass = True
+        if word in fence_strings:
+            fence = True
+        if word.isnumeric():
+            if x_coordinate != -1 and 1 <= int(word) <= 5:
+                coordinates.append((x_coordinate, int(word)))
+                x_coordinate = -1
+            elif x_coordinate == -1 and 1 <= int(word) <= 8:
+                x_coordinate = int(word)
+        if word in eat_strings:
+            to_eat = True
+
+    if to_eat:
+        result = ""
+        current_animals = my_farm.get_animals()
+        grasses = my_farm.get_grasses()
+        for animal in animals:
+            if animal not in current_animals:
+                return "The " + animal + " is not in your farm currently"
+        animals_in_the_sent = animals.copy()
+        while animals:
+            animal = animals.pop(0)
+            if animal in herbivores:
+                if not grasses:
+                    result += "The " + animal + " does not find any grass"
+                elif not intersection(food_chain2[animal], animals_in_the_sent):
+                    result += "The " + animal + " is eating grass"
+                continue
+            food = intersection(food_chain[animal], current_animals)
+            if not food:
+                result += "The " + animal + " does not find its prey"
+            else:
+                result += "The " + animal + " is eating " + food[0]
+        return result
+
+    if animals and is_adding:
+        result = ""
+        for animal in animals:
+            new_animal = Animal(animal)
+            if coordinates:
+                coord = coordinates.pop(0)
+                my_farm.add_animal(new_animal, coordinate=coord)
+                result += "A " + animal + " has been added to your farm at " + str(coord) + " "
+            else:
+                my_farm.add_animal(new_animal, True)
+                result += "A " + animal + " has been added to your farm at a random position "
+        return result
+
+    if grass and is_adding:
+        result = ""
+        if not coordinates:
+            my_farm.add_grass_randomly()
+            return "A grass has been added to your farm at a random position "
+        for coordinate in coordinates:
+            my_farm.add_grass(coordinate)
+            result += "A grass has been added to your farm at " + str(coordinate) + " "
+        return result
+
+    if fence and is_adding:
+        result = ""
+        if not coordinates:
+            my_farm.add_fence_randomly()
+            return "A fence has been added to your farm at a random position "
+        for coordinate in coordinates:
+            my_farm.add_fence(coordinate)
+            result += "A fence has been added to your farm at " + str(coordinate) + " "
+        return result
+
+
+def intersection(lst1, lst2):
+    lst3 = [value for value in lst1 if value in lst2]
+    return lst3
